a2305f224e65a72e77d6fe388bdd75911f4054da
Dylan Wolff <dylanwolff@gmail.com>
2015-02-25 19:34:19 +1100

    Redirect all URLs for non-logged in users to root_url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :require_login
+
+  private
+
+  def require_login
+    unless current_user
+      redirect_to root_url
+    end
+  end
+
 end
