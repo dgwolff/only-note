c5703e731ba62ca8453d07a32c07a2969487b917
Dylan Wolff <dylanwolff@gmail.com>
2015-02-26 16:21:06 +1100

    Revert "Install omniauth gem, add provider & id columns to Users"
    This reverts commit 12e74be602b4ec2d819d299f6e4db562019cd5c5.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226043231) do
+ActiveRecord::Schema.define(version: 20150225015926) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
@@ -34,8 +34,6 @@ ActiveRecord::Schema.define(version: 20150226043231) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "provider"
-    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
